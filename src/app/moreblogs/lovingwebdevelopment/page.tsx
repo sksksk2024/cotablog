13662dc9ab47a4e84f034ac160da77f7b0f2ff95
Alex <cota8091@gmail.com>
2025-05-08 20:41:05 +0300

    &apos; expression in html for '

--- src/app/moreblogs/lovingwebdevelopment/page.tsx
+++ src/app/moreblogs/lovingwebdevelopment/page.tsx
@@ -22,17 +22,17 @@ const WebDevelopmentBlog = () => {
         <h2 className="text-xl underline">🚀 Discovering Web Development</h2>
         <p>
           My Web Development started to shine when I wanted to get to the
-          market, in someway. I didn't want to do any more physical activities
-          in addition to my difficult workouts and going to school. I wanted to
-          make money from home. One time, I saw trading as an option, but then
-          gave up, because I don't like to spend time learning something just
-          for money, but also for my near future business. Then Web Development
-          came as my second option.
+          market, in someway. I didn&apos;t want to do any more physical
+          activities in addition to my difficult workouts and going to school. I
+          wanted to make money from home. One time, I saw trading as an option,
+          but then gave up, because I don&apos;t like to spend time learning
+          something just for money, but also for my near future business. Then
+          Web Development came as my second option.
         </p>
         <p>
           I realised how much you have to pay a web developer, even more than
-          any other role in a business, in my oppinion. I didn't want to pay
-          someone that much for my business, obviously not in my start of my
+          any other role in a business, in my oppinion. I didn&apos;t want to
+          pay someone that much for my business, obviously not in my start of my
           journey. So, I started to practise making sites, and with the same
           cycle that I did with calisthenics, I got as much information as
           possible while applying it along the way.
@@ -43,10 +43,10 @@ const WebDevelopmentBlog = () => {
           by how hard HTML language was, to be able to learn and become a
           fullstack web developer in Nextjs, Prisma, Reactjs, Tailwind CSS,
           TypeScript, Framer Motion, and having experience with lots of other
-          technologies, that I taught they won't treat my dreams as I want, but
-          at the same time, they pushed me faster to my wanted stack. To give
-          you some tech examples, they are: MongoDB, Nodejs, Vitest, Docker,
-          Vue, CSS, SCSS/SASS, Ruby on Rails, GSAP, ... a lot more.
+          technologies, that I taught they won&apos;t treat my dreams as I want,
+          but at the same time, they pushed me faster to my wanted stack. To
+          give you some tech examples, they are: MongoDB, Nodejs, Vitest,
+          Docker, Vue, CSS, SCSS/SASS, Ruby on Rails, GSAP, ... a lot more.
         </p>
         <h2 className="text-xl underline">
           📚 My learning journey - in more depth
@@ -55,23 +55,24 @@ const WebDevelopmentBlog = () => {
           My brother, who is a web developer himself, gave me this opportunity
           to learn this industry and, also, he said to start learning Ruby on
           Rails, which focuses on the functionality of a website, also known as
-          the backend. Looking back, this was a misstep: I couldn't learn much,
-          I was stuck in so many problems: how to deploy this on github, how to
-          make the website cooler looking, how to add multiple functionalities,
-          without breaking my head, and the cherry at the top, how to not hope
-          that some random JavaScript file worked without knowing why. I didn't
-          understand anything: it was a combination with also html, bootstrap
-          css classes, and JavaScript, which was a real mess. After a few
-          months, I got separated from that violating environment, thinking
-          learning the actual basics would help me. My learning path was, and I
-          also suggest for those who want to learn how to put fascinating pages
-          on the browser: html, css, JavaScript, css frameworks, JavaScript
-          frameworks, typescript, then the backend. My path was, for that time,
-          a brilliant set of moves that I took, which personally, skyrocketed my
-          expectations on making cooler and better websites, overall. At the
-          same time, the process got fast and efficient, while facing fewer
-          struggles. After this adventure, I didn't come back to the tech that
-          my brother uses, since I like my tech stack much more.
+          the backend. Looking back, this was a misstep: I couldn&apos;t learn
+          much, I was stuck in so many problems: how to deploy this on github,
+          how to make the website cooler looking, how to add multiple
+          functionalities, without breaking my head, and the cherry at the top,
+          how to not hope that some random JavaScript file worked without
+          knowing why. I didn&apos;t understand anything: it was a combination
+          with also html, bootstrap css classes, and JavaScript, which was a
+          real mess. After a few months, I got separated from that violating
+          environment, thinking learning the actual basics would help me. My
+          learning path was, and I also suggest for those who want to learn how
+          to put fascinating pages on the browser: html, css, JavaScript, css
+          frameworks, JavaScript frameworks, typescript, then the backend. My
+          path was, for that time, a brilliant set of moves that I took, which
+          personally, skyrocketed my expectations on making cooler and better
+          websites, overall. At the same time, the process got fast and
+          efficient, while facing fewer struggles. After this adventure, I
+          didn&apos;t come back to the tech that my brother uses, since I like
+          my tech stack much more.
         </p>
         <h2 className="text-xl underline">
           🏆 My Greatest Achievements in Web Development - by now
@@ -89,19 +90,19 @@ const WebDevelopmentBlog = () => {
           easier. I guarantee you: if you put me on the desk working for one
           month, I will accomplish more than I learned in one year, and this
           amount of time will only increase if I continue to work as hard as
-          possible. In my case, my goal wasn't to become one of the best in the
-          industry, but rather just feeling comfortable with the skill, so I can
-          move on to other things to master in these teenage years and that are
-          vital, such as communication, writing, and sales.
+          possible. In my case, my goal wasn&apos;t to become one of the best in
+          the industry, but rather just feeling comfortable with the skill, so I
+          can move on to other things to master in these teenage years and that
+          are vital, such as communication, writing, and sales.
         </p>
         <h2 className="text-xl underline">💡 Was It worth It?</h2>
         <p>
           Definetly. I loved to discover my creativity and to develop the skill
           of solving problems through web development. This journey taught me to
           be conscious not to select multiple steps at a time, and that the
-          limits don't exist, while you can still be awake, energetic and with
-          the purpose to finish the work in that specific day. Also, now I have
-          lots of opportunities unlocked. Besides being able to create my
+          limits don&apos;t exist, while you can still be awake, energetic and
+          with the purpose to finish the work in that specific day. Also, now I
+          have lots of opportunities unlocked. Besides being able to create my
           personal websites, now I am focused to take a job in the industry. I
           will try my best to get it, as I know it will boost me in things such:
           collaboration, teamwork, making new friends that are also driven by
